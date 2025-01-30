1ad8e59937d258dc51ddef679b96266692b68c3b
Sara Kazemi <sara.kazemi@okta.com>
2025-01-30 21:53:59 +0000

    Add up to Lab 2-2

--- ice-resource-server/config.js
+++ ice-resource-server/config.js
@@ -1,6 +1,6 @@
 // config.js
 export default {
-  issuer: "https://oktaiceXXXXXXXX.oktapreview.com/oauth2/aXXXXXXXXXXXX",
+  issuer: "https://{domain}.okta.com/oauth2/aXXXXXXXXXXXX",
   audience: "api://promos",
   clientId: "okta.client.id",
 };
--- ice-spa-app/src/config.js
+++ ice-spa-app/src/config.js
@@ -1,10 +1,10 @@
 // config.js
 export default {
   oidc: {
-    clientId: "XXXXXXXXXXXXXXXXXX",
-    issuer: "https://oktaiceXXXXXXXX.oktapreview.com",
+    clientId: "XXXXXXXXXXXXXXXXXX", // 👈 update with your Client ID
+    issuer: "https://{domain}.okta.com", // 👈 update with your Okta Org URL
     redirectUri: window.location.origin + "/login/callback",
-    // authorizationEndpoint: "https:// oktaiceXXXXXXXX.oktapreview.com/oauth2/axxxxxxxxxxxx/v1/authorize",
+    // authorizationEndpoint: "https://{domain}.okta.com/oauth2/axxxxxxxxxxxx/v1/authorize",
     scopes: ["openid", "profile", "email"],
     tokenManager: {
       storage: "localStorage",
