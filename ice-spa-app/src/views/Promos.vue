bc010f33e9d41eb8cc5436b02b204d651da1b796
Sara Kazemi <sara.kazemi@okta.com>
2025-02-05 00:59:48 +0000

    Update API_URL for codespaces

--- ice-spa-app/src/views/Promos.vue
+++ ice-spa-app/src/views/Promos.vue
@@ -52,7 +52,10 @@
 // @ is an alias to /src
 import Footer from "@/components/Footer.vue";
 // import {getAuthHeader} from "@/main.js"
-const API_URL = "http://localhost:8081";
+const API_PORT = 8081;
+const API_URL = process.env.VUE_APP_CODESPACES
+  ? `https://${process.env.VUE_APP_CODESPACE_NAME}-${API_PORT}.${process.env.VUE_APP_GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}`
+  : `http://localhost:${API_PORT}`;
 
 export default {
   name: "Promos",
